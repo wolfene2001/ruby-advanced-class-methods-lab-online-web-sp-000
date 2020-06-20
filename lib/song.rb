8c2c82904f3aedb60b230a9eecd781b8428f9537
DEE Hurd <wolfene2001@yahoo.com>
2020-06-20 17:54:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,7 @@ def self.new_from_filename(filename)
   new_file = filename.split(" - ")
   artist_name = new_file[0]
   song_name = new_file[1].gsub(".mp3", " ")
-  
+  song = self.new
   
  
 def save
