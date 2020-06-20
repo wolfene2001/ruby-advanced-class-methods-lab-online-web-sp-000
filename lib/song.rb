2ae25da17951fdab1770d2561b9243ceac393c7c
DEE Hurd <wolfene2001@yahoo.com>
2020-06-20 18:02:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,16 @@ def self.new_from_filename(filename)
   song
 end
 
-def self.create_from_filename
+def self.create_from_filename(filename)
+  new_file = filename.split(" - ")
+  artist_name = new_file[0]
+  song_name = new_file[1].gsub(".mp3", "")
+  song = self.create
+  song.name = song_name
+  song.artist_name = artist_name
+  song
+end
+
 def save
   self.class.all << self
 end
